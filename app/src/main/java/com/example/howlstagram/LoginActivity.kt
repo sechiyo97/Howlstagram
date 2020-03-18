51fd0bb2a27b6d6d9c00b6ede9fc15abcd5d241e
sechiyo97 <sechiyo97@gmail.com>
2020-03-19 01:50:21 +0900

    google, facebook login

--- app/src/main/java/com/example/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram/LoginActivity.kt
@@ -14,23 +14,26 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
-import com.google.android.gms.common.util.IOUtils.toByteArray
 import android.content.pm.PackageManager
-import android.content.pm.PackageInfo
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.util.Base64
 import android.util.Log
-import androidx.fragment.app.FragmentActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth?= null
     var googleSignInClient : GoogleSignInClient?= null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager : CallbackManager?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -42,13 +45,19 @@ class LoginActivity : AppCompatActivity() {
             // First step
             googleLogin()
         }
+        facebook_sign_in_button.setOnClickListener{
+            // First step
+            facebookLogin()
+        }
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-        printHashKey()
+        //printHashKey()
+        callbackManager = CallbackManager.Factory.create()
     }
+    /*
     fun printHashKey() {
         try {
             val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
@@ -64,14 +73,51 @@ class LoginActivity : AppCompatActivity() {
             Log.e("TAG", "printHashKey()", e)
         }
 
-    }
+    }*/
     fun googleLogin(){
         var signInIntent = googleSignInClient?.signInIntent
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
+    fun facebookLogin(){
+        LoginManager.getInstance()
+                .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+                .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                    // Second step
+                    override fun onSuccess(result: LoginResult?) {
+                        handleFacebookAccessToken(result?.accessToken)
+                    }
+
+                    override fun onCancel() {
+                        // TO_DO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        // TO_DO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                })
+    }
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+                ?.addOnCompleteListener{
+                    task ->
+                    if(task.isSuccessful){
+                        // Third step
+                        // login
+                        moveMainPage(task.result!!.user)
+                    } else{
+                        // show the error message
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
         if(requestCode == GOOGLE_LOGIN_CODE){
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if(result.isSuccess){
